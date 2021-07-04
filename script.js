bb1438fdc4f0b962e69641d750e84b5d3d801d74
Rukotana Freddy <rukotanafreddy@gmail.com>
2021-07-04 23:55:44 +0200

    initial commit

--- script.js
+++ script.js
@@ -4,20 +4,36 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-const tickectPrice = +movieSelect.value;
+let tickectPrice = +movieSelect.value;
 console.log(tickectPrice);
+
+// local starage 
+// const memory = localStorage.setItem('seats');
 
 // update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    console.log();
+    // copy total and count
+    const seatsIndex = [...selectedSeats].map(function(seat){
+        return [...seats].indexOf(seat);
+    });
+    console.log(seatsIndex);
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     console.log(selectedSeats);
+    
     count.innerText = selectedSeats.length;
     total.innerText = selectedSeats.length * tickectPrice;
 }
 
 // Movie oku
 movieSelect.addEventListener('change', e => {
-    tickectPrice = e.target.value;
+    tickectPrice = +e.target.value;
+    console.log(e.target.selectedIndex);
+    updateSelectedCount();
+    console.log(e.target.value);
 });
 
 
